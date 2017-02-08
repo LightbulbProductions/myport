1feff52c5530d94c7012cc9d27d4788d7bb98210
LightbulbProductions <gerbermichelle23@gmail.com>
2017-02-08 09:57:21 -0500

    portfolio destroy button

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -22,7 +22,11 @@ class PortfoliosController < ApplicationController
   def edit
     @portfolio_item = Portfolio.find(params[:id])
   end
-
+  
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+  
   def update
     @portfolio_item = Portfolio.find(params[:id])
     
@@ -32,6 +36,15 @@ class PortfoliosController < ApplicationController
       else
         format.html { render :edit }
       end
+    end
+  end
+  
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Annihilated!' }
     end
   end
 
