5b5144c1bb089486508a82665224819dca7b4c4e
LightbulbProductions <gerbermichelle23@gmail.com>
2017-02-11 14:19:48 -0500

    integrated custom copyright gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end 
+  
+  def copyright_generator
+    SiteViewTool::Renderer.copyright 'Michelle Gerber', 'All Rights Reserved'
+  end
 end
