44c49b1a3b43e8f21c7f595fcd05aecbf8ae1f83
LightbulbProductions <gerbermichelle23@gmail.com>
2017-02-10 20:14:42 -0500

    obj null pattrn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
+  
+  def current_user
+    super || OpenStruct.new(name: "Guest", first_name: "Guest", last_name: "Guest", email: "guest@guest.com")
+  end
 end
