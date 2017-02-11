8d365ce07c78dcf08c557402ad379cbda415eb38
LightbulbProductions <gerbermichelle23@gmail.com>
2017-02-10 20:18:22 -0500

    refactored openstruct into concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
-  
-  def current_user
-    super || OpenStruct.new(name: "Guest", first_name: "Guest", last_name: "Guest", email: "guest@guest.com")
-  end
+  include CurrentUserConcern
 end
