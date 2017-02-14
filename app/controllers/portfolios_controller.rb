4f53c693d330be06b75e0846f4c4d9c3c78d4b24
LightbulbProductions <gerbermichelle23@gmail.com>
2017-02-14 12:54:56 -0500

    added scope to portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,7 +3,7 @@ class PortfoliosController < ApplicationController
   layout "portfolio"
   before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
 
   def new
